0a58148744b57f3308e85874f7c7753854c3db1a
uygarpolat <uygarpolat@gmail.com>
2026-01-16 21:03:36 +0200

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import { KeyboardContextProvider } from './Store/keyboard-context'
 import './index.css'
@@ -7,5 +8,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <KeyboardContextProvider>
     <App />
+    <Analytics />
   </KeyboardContextProvider>
 )
